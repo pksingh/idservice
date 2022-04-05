c2930edb03a164809dac11ed4621c899d4f38b28
Prasanta Singh <singh.prasanta@gmail.com>
2022-04-05 20:28:17 +0530

    Added TC for NextId() on snowid package

---
+++ snowid/snowid_test.go
@@ -0,0 +1,28 @@
+package snowid
+
+import (
+	"testing"
+)
+
+func TestNextIds(t *testing.T) {
+	SetDefaultNode()
+
+	next1 := NextId()
+	next2 := NextId()
+
+	if next1 == next2 {
+		t.Errorf("two snowids are equal: %v", next1)
+	}
+
+	if next2 < next1 {
+		t.Errorf("two snowids are generated out of order, diff(next2-next1): %v", (next2 - next1))
+	}
+
+	next3 := NextId()
+	if next3 < next1 {
+		t.Errorf("two snowids are generated out of order, diff(next3-next1): %v", (next3 - next1))
+	}
+	if next3 < next2 {
+		t.Errorf("two snowids are generated out of order, diff(next3-next2): %v", (next3 - next2))
+	}
+}
